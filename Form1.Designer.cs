1b119c79b6966344dd7ef3d77bca85f9af86fc83
maha othman <yeolbyun2002@gmail.com>
2023-10-15 19:36:36 +0300

    gsfd

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace XOProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(357, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic-Tac-Toe Game";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox1.Location = new System.Drawing.Point(906, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "?";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox2.Location = new System.Drawing.Point(685, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "?";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox3.Location = new System.Drawing.Point(460, 158);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "?";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox4.Location = new System.Drawing.Point(460, 323);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "?";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox5.Location = new System.Drawing.Point(685, 323);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "?";
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox6.Location = new System.Drawing.Point(906, 323);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 4;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "?";
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox7.Location = new System.Drawing.Point(460, 491);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "?";
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox8.Location = new System.Drawing.Point(685, 491);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "?";
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::XOProject.Properties.Resources.QuistionMark;
+            this.pictureBox9.Location = new System.Drawing.Point(906, 491);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(185, 125);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 7;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "?";
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(12, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 60);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Turn:";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTurn.Location = new System.Drawing.Point(15, 234);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(234, 60);
+            this.lblTurn.TabIndex = 11;
+            this.lblTurn.Tag = "Player1";
+            this.lblTurn.Text = "Player1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(12, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 60);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Winner:";
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblWin.Location = new System.Drawing.Point(20, 403);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(337, 60);
+            this.lblWin.TabIndex = 13;
+            this.lblWin.Tag = "In Progress";
+            this.lblWin.Text = "In Progress";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(110, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 72);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Restart Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1141, 681);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "X O Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWin;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XOProject.Properties;
+
+namespace XOProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.White;
+            Pen pen = new Pen(White);
+
+            pen.Width = 8;
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+           // e.Graphics.DrawLine(pen, 300, 100, 300, 400);
+
+            //Line Vertical
+            e.Graphics.DrawLine(pen, 450, 100, 450, 400);
+
+            e.Graphics.DrawLine(pen, 600, 100, 600, 400);
+
+            //Line Horizental
+            e.Graphics.DrawLine(pen, 300, 190, 740, 190);
+
+            e.Graphics.DrawLine(pen,  300, 300, 740, 300);
+
+        }
+
+        void RestartGaming()
+        {
+            lblTurn.Text = lblTurn.Tag.ToString();
+            lblWin.Text = lblWin.Tag.ToString();
+            Count = 0;
+            pictureBox1.Image = Resources.QuistionMark;
+            pictureBox2.Image = Resources.QuistionMark;
+            pictureBox3.Image = Resources.QuistionMark;
+            pictureBox4.Image = Resources.QuistionMark;
+            pictureBox5.Image = Resources.QuistionMark;
+            pictureBox6.Image = Resources.QuistionMark;
+            pictureBox7.Image = Resources.QuistionMark;
+            pictureBox8.Image = Resources.QuistionMark;
+            pictureBox9.Image = Resources.QuistionMark;
+
+            pictureBox1.Tag = "?";
+            pictureBox2.Tag = "?";
+            pictureBox3.Tag = "?";
+            pictureBox4.Tag = "?";
+            pictureBox5.Tag = "?";
+            pictureBox6.Tag = "?";
+            pictureBox7.Tag = "?";
+            pictureBox8.Tag = "?";
+            pictureBox9.Tag = "?";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestartGaming();
+        }
+
+        void ChangeImage()
+        {
+
+        }
+        void Winner()
+        { 
+            if( ((pictureBox1.Tag.ToString() == "1") &&
+                 (pictureBox2.Tag.ToString() == "1") &&
+                 (pictureBox3.Tag.ToString() == "1"))||
+
+                ((pictureBox4.Tag.ToString() == "1") &&
+                 (pictureBox5.Tag.ToString() == "1") &&
+                 (pictureBox6.Tag.ToString() == "1"))||
+
+                ((pictureBox7.Tag.ToString() == "1")&&
+                 (pictureBox8.Tag.ToString() == "1") &&
+                 (pictureBox8.Tag.ToString() == "1"))||
+
+                ((pictureBox1.Tag.ToString() == "1") &&
+                (pictureBox6.Tag.ToString() == "1") &&
+                (pictureBox9.Tag.ToString() == "1")) ||
+
+                ((pictureBox2.Tag.ToString() == "1") &&
+                (pictureBox5.Tag.ToString() == "1") &&
+                (pictureBox8.Tag.ToString() == "1")) ||
+
+                ((pictureBox3.Tag.ToString() == "1") &&
+                (pictureBox4.Tag.ToString() == "1") &&
+                (pictureBox7.Tag.ToString() == "1")) ||
+
+                ((pictureBox3.Tag.ToString() == "1") &&
+                (pictureBox5.Tag.ToString() == "1") &&
+                (pictureBox9.Tag.ToString() == "1")) ||
+
+               ((pictureBox1.Tag.ToString() == "1") &&
+                (pictureBox5.Tag.ToString() == "1") &&
+                (pictureBox7.Tag.ToString() == "1")))
+            {
+                lblWin.Text = "Player1";
+                MessageBox.Show("Player1 Is Winner", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Count = 9;
+
+                return;
+            }
+            if (((pictureBox1.Tag.ToString() == "2") &&
+                 (pictureBox2.Tag.ToString() == "2") &&
+                 (pictureBox3.Tag.ToString() == "2")) ||
+
+                ((pictureBox4.Tag.ToString() == "2") &&
+                 (pictureBox5.Tag.ToString() == "2") &&
+                 (pictureBox6.Tag.ToString() == "2")) ||
+
+                ((pictureBox7.Tag.ToString() == "2") &&
+                 (pictureBox8.Tag.ToString() == "2") &&
+                 (pictureBox9.Tag.ToString() == "2")) ||
+
+                ((pictureBox1.Tag.ToString() == "2") &&
+                 (pictureBox6.Tag.ToString() == "2") &&
+                 (pictureBox9.Tag.ToString() == "2")) ||
+
+                ((pictureBox2.Tag.ToString() == "2") &&
+                 (pictureBox5.Tag.ToString() == "2") &&
+                 (pictureBox8.Tag.ToString() == "2")) ||
+
+                ((pictureBox3.Tag.ToString() == "2") &&
+                 (pictureBox4.Tag.ToString() == "2") &&
+                 (pictureBox7.Tag.ToString() == "2")) ||
+
+                ((pictureBox3.Tag.ToString() == "2") &&
+                 (pictureBox5.Tag.ToString() == "2") &&
+                 (pictureBox9.Tag.ToString() == "2")) ||
+
+                ((pictureBox1.Tag.ToString() == "2") &&
+                 (pictureBox5.Tag.ToString() == "2") &&
+                 (pictureBox7.Tag.ToString() == "2")))
+            {
+                lblWin.Text = "Player2";
+                MessageBox.Show("Player2 Is Winner", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Count = 9;
+
+                return;
+            }
+            if ((pictureBox1.Tag.ToString() !="?") &&
+                (pictureBox3.Tag.ToString() !="?") &&
+                (pictureBox2.Tag.ToString() !="?") &&
+                                              
+               (pictureBox4.Tag.ToString()  !="?") &&
+                (pictureBox5.Tag.ToString() !="?") &&
+                (pictureBox6.Tag.ToString() !="?") &&
+                                             
+               (pictureBox7.Tag.ToString()  !="?") &&
+                (pictureBox8.Tag.ToString() !="?") &&
+                (pictureBox9.Tag.ToString() !="?"))
+            {
+                lblWin.Text = "No Winner";
+                MessageBox.Show("No Winner", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Count = 9;
+                return;
+            }
+        }
+        short Count = 0;
+        void ConvertPBox(PictureBox  PB)
+        {
+            if (PB.Tag.ToString() == "?" && Count < 9)
+            {
+                if (lblTurn.Text == "Player1")
+                {
+                    PB.Image = Resources.X;
+                    lblTurn.Text = "Player2";
+                    PB.Tag = 1;
+                    Count++;
+                }
+                else
+                {
+                    PB.Image = Resources.O;
+                    lblTurn.Text = "Player1";
+                    PB.Tag = 2;
+                    Count++;
+                }
+
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            ConvertPBox((PictureBox)sender);
+            Winner();
+
+        }
+    }
+}
